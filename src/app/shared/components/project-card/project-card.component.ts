de7bc51e1a9d1234ea40d1ee7406fdd8e0ed51b4
Ali Hussein <ali.hussein@live.com>
2025-09-28 22:47:15 +0300

    Updated skills for project cards

--- src/app/shared/components/project-card/project-card.component.ts
+++ src/app/shared/components/project-card/project-card.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject, Input } from '@angular/core';
 import { IProject } from '../../../core/types/project.interface';
 import { TranslatePipe } from '@ngx-translate/core';
-import { NgClass } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { LocalizationService } from '../../../core/services/localization.service';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [TranslatePipe, NgClass],
+  imports: [TranslatePipe, NgClass, CommonModule],
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
